93b09b1f26962ba177de1ff7070e3e4c08310c6c
camila2108 <gutierrezcamilamilagros02@gmail.com>
2022-12-13 02:00:50 -0300

    subiendo no static img

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
    return res.sendFile(path.join(__dirname, "/views/home.html"));
